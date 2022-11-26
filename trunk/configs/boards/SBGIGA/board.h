9374f900349959a1905512aaebcd3f770d437de2
r3d5ky <46210207+r3d5ky@users.noreply.github.com>
2022-11-26 06:14:01 +0000

    Add initial files for Smartbox Giga

---
+++ trunk/configs/boards/SBGIGA/board.h
@@ -0,0 +1,20 @@
+/* Beeline Smartbox Giga */
+
+#define BOARD_PID		"SBGIGA"
+#define BOARD_NAME		"SBGIGA"
+#define BOARD_DESC		"Beeline SmartBox Giga"
+#define BOARD_VENDOR_NAME	"Beeline"
+#define BOARD_VENDOR_URL	"http://www.beeline.ru/"
+#define BOARD_MODEL_URL		"http://www.beeline.ru/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120	
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3		1
